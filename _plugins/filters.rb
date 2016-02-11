48287365d9f5399ce961353ba712d1695935e6df
Takuya Tsuchida <takuya.tsuchida@gmail.com>
2016-02-11 10:57:36 +0900

    そりゃまあ任意のコードなんて実行できるわけないよなぁ。。。

--- _plugins/filters.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-  module Filters
-    def sort_tags_list(tags)
-      #sorted_tags = tags.keys.sort_by! { |tag| tag.downcase }
-      tags.sort { |a, b| b[1].size <=> a[1].size }
-    end
-  end
-end
